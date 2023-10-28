2eb2e6c0d91237aabbfbd672597ddb7a368fe8e8
Pankaj <pankajbeniwal3112@gmail.com>
2023-10-28 11:29:14 +0530

    add redirect url

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ router.get(
 router.get(
   "/login",
   passport.authenticate("google", {
-    successRedirect: "http://localhost:3000",
+    successRedirect: "https://btech-burger-wala-sigma.vercel.app",
   })
 );
 
